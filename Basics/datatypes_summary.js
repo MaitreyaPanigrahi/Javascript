39816e4ad4bc10d21a42a184cdaca6046d0cf72a
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-27 05:47:11 +0000

    Stack & Heap Concept

--- Basics/datatypes_summary.js
+++ Basics/datatypes_summary.js
@@ -29,11 +29,34 @@ const myFunc = function add(a,b){
     return a+b
 }
 
-console.log(typeof temp); // Null -> object
-console.log(typeof userEmail); // undefined -> undefined
-console.log(typeof heros); // array -> object
-console.log(typeof myFunc); // function -> function object
-console.log(typeof myobj); //  objects -> objects
+// console.log(typeof temp); // Null -> object
+// console.log(typeof userEmail); // undefined -> undefined
+// console.log(typeof heros); // array -> object
+// console.log(typeof myFunc); // function -> function object
+// console.log(typeof myobj); //  objects -> objects
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+// Stack(Primitive)  ,Heap(Non-Primitive)
+
+let myName = 'Maitreya';
+let anotherName = myName; // Only the value of myName is 
+// given to anotherName. Any change is anotherName 
+// is not reflected in myName . A copy is of myName is created
+anotherName = 'tubu'
+// console.log(anotherName);
+// console.log(myName);                        STACK                                      HEAP
+                                            
+let userOne = {                                 //userOne  ------------------------------>
+    email:'user@g.com',                         //userTwo  ------------------------------> // email:'user@g.com',
+    upi:'mp@ybl'                               //anotherName = myName                     // upi:'mp@ybl'        
+}                                             //myName = 'Maitreya'                   
+
+userTwo = userOne; // userOne reference / address
+ // is passes onto userOne. Any change in userTwo 
+ // is reflected in userOne
+userTwo.email = "mp@gmail.com";
+console.log(userTwo);
+console.log(userOne);
 
 
 
