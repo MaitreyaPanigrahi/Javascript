c2e8ec1526f2e9bb623aa964b71d86e75d56ef2f
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-27 02:26:05 +0000

    Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all JS Code as newer version
+
+// alert('hello'); We are using node js, not browser
+
+// console.log(3 + 3);
+
+let name = 'maitreya'; // String Data type
+let age = 18; // Number Data Type
+
+let isloggedIn = true // Boolean Data type
+
+/*Data type - Primitive
+number - 2 to power 53
+bigint
+string - ""
+boolean - true or false
+null - standalone value
+undefined -  
+symbol - unique
+*/
+
+//Object
+
+console.log(typeof 'maitreya');
+console.log(typeof age);
+console.log(typeof isloggedIn);
+console.log(typeof null); // Object
+console.log(typeof undefined); // Undefined
+
