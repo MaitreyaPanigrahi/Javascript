a31a77a952660f70207ce001f41370d5beaf7b6d
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-27 14:28:23 +0000

    Change is Block Scope and Functional Scope

--- Basics/variables.js
+++ Basics/variables.js
@@ -17,6 +17,24 @@ console.log(accountId);
 /*
 Prefer Not to use var because of 
 the issue in block scope and functional scope 
+
+Block Scope
+
+function example() {
+    if (true) {
+        var x = 10; // `x` is accessible within the if block
+    }
+    console.log(x); // 10
+}
+example();
+ 
+Functional Scope
+
+function example() {
+    var x = 10; // `x` is accessible within the `example` function
+    console.log(x); // 10
+}
+example();
 */
 
 console.table([emailId,accountId,accountPassword,accountCity,accountState]);
