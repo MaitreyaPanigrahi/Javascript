bfd7187fa91854e258d3c80842c3559b50ed5504
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-27 11:29:50 +0000

    Strings commit

---
+++ Basics/Strings.js
@@ -0,0 +1,52 @@
+// const names = "hitesh";
+// const repoCount = 50;
+
+// console.log(names + repoCount + "Value"); - Outdated
+
+// console.log(`Hello my name is ${names} and my repoCount is${repoCount}`)
+
+// const gameName = new String('maitreya-P');
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,5);
+// console.log(anotherString);
+
+// const newString1 = "     maitreya       ";
+// console.log(newString.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+
+// console.log(url.replace('%20',"-"));
+
+// console.log(url.includes('hitesh'));
+
+// +++++++ SOME IMPORTANT STRING METHODS
+
+// const oneString = "mait-pani-tubu-shri";
+// console.log(oneString.split('-'));
+// console.log(oneString.at(-3)); // Return the character at index 3.
+// If negative value given then starts from the end indexing from -1
+
+// console.log(oneString.charAt(-3)); //Same as .at but doesn't support negative values
+
+let nameOne = "mait";
+let nameTwo = "pani";
+// console.log(nameOne.concat(' ',nameTwo)); // Add two strings
+
+// console.log(nameOne.endsWith('it')); // Returns true or false Its case-sensitive
+
+let sent = "my name is maitreya";
+// console.log(sent.includes("is")); // Returns true or false.Its case-sensitive
+
+
+
+
