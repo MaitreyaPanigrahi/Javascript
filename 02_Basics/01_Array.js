d12ded602b102b142e7238964085291fda55d04d
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-28 05:27:52 +0000

    Arrays and it Methods

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,35 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["shaktiman", "naagraj"];
+const myArr2 = new Array(1, 2, 3, 4);
+// console.log(myArr[3]);
+
+//Array Methods
+
+// myArr.push(99);
+// myArr.push(96);
+// myArr.pop()
+
+// myArr.unshift(-1) // Shift all element by 1 and enters an element at first
+// myArr.shift() // Remove the first element
+
+// console.log(myArr.includes(9)); // Return true or false
+// console.log(myArr.indexOf(9)); // Return index else return -1 if absent
+
+// const newArr = myArr.join()
+
+// console.log(myArr);// [01,2,3,4,5]
+// console.log( newArr); // 0,1,2,3,4,5
+// console.log(typeof newArr); // String
+
+// slice ,splice
+
+console.log("A", myArr); // [0,1,2,3,4,5]
+
+const myN1 = myArr.slice(1, 3); // 1 included nd 3 excluded. Return the portion.No impact on original array
+console.log(myN1);  // [1,2]
+console.log("B", myArr); // [0,1,2,3,4,5]
+
+const myN2 = myArr.splice(1, 3); // 1 nd 3 included.Cut the portion from original array and return it.Have impact on original array
+console.log(myN2); // [1,2,3]
+console.log("C", myArr); // [0,4,5]
+
---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,31 @@
+const marvelHeros = ["thor", "ironman", "spiderman"];
+const dcHeros = ["superman", "flash", "batman"];
+
+// const newHero = marvelHeros.push(dcHeros);
+
+// marvelHeros.push(dcHeros);
+// console.log(marvelHeros);
+
+//  marvelHeros.concat(dcHeros);
+// console.log(marvelHeros); // Changes in original Array
+
+const heros = [...marvelHeros,...dcHeros,...[1,2,3,4]] // Concat all the arrays into single array
+// Spread Operator(...)
+// console.log(heros);
+
+const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+
+const real_another_arr = anotherArray.flat(2) // Convert all the nested elements into single array
+// console.log(real_another_arr);
+
+// console.log(Array.isArray('maitreya')) // Return True of False
+// console.log(Array.from('maitreya')) // Converts to an arra
+console.log(Array.from(Object.keys({nams:'maitreya',class :2})))  // Interesting case
+
+let score1 = 100
+let score2 = 200
+let score3 = 500
+
+console.log(Array.of(score1,score2,score3));
+
