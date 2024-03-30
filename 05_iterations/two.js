bf674be9f480f6c5776ff93366a76de445fb5e0c
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-30 11:54:46 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// let i = 1
+// while (i<10) {
+//     console.log(i);
+//     i+=2;
+// }
+
+let myArr = ['flash','batman','superman'];
+// let index = 0;
+// while(index<myArr.length){
+//     console.log(`Superhero is ${myArr[index]}`);
+//     index++
+// }
+
+// do-while
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=1);
