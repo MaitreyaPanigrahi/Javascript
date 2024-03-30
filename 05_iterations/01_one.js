fbaae64a4124a7ff5aec6e32cede35f6b8cd7cd1
MaitreyaPanigrahi <91478557+MaitreyaPanigrahi@users.noreply.github.com>
2024-03-30 11:29:40 +0000

    for loop

--- 05_iterations/01_one.js
+++ 05_iterations/01_one.js
@@ -1,4 +1,57 @@
+<<<<<<< HEAD
 for (let i = 0; i < 10; i++) {
     console.log(i);
     
 }
+=======
+// for loop
+
+// for (/*1 */ let i = 0; /*2 */ i<=10; /*3 */ i++){
+//     const element = i; /*4 */
+//     if(element == 5){
+//         console.log(`Hello i am ${element}`);
+//     }
+//     console.log(element)
+// }
+
+// Steps - 1 -> 2 -> 4 -> 3 -> 2 -> 4 ..... 
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//     //    console.log(`Inner loop value ${j} and inner loop ${i}`);
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+    
+// }
+
+let myArr = ["Flash","Batman" , "Superman"]
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+    
+}
+
+// break and continue
+
+// for (let index = 1; index <=20; index++) {
+//     if(index === 5){
+//         console.log(`5 detected !`);
+//         break;
+//     }
+//     console.log(`Value is ${index}`);
+
+    
+// }
+for (let index = 1; index <=20; index++) {
+    if(index === 5){
+        console.log(`5 detected !`);
+        continue;
+    }
+    console.log(`Value is ${index}`);
+
+    
+}
+>>>>>>> b12d498 (for loop)
